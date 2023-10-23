fe3790ec833f4d4f225fe2a6b26e38ca647bfb33
Chrisanto76 <antonypoure2@gmail.com>
2023-10-23 11:51:30 +0200

    add recipe-api.ts

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,15 +1,19 @@
 import express from 'express';
 import cors from 'cors';
+import * as RecipeAPI from './recipe-api';
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 
-app.get('/api/recipe/search', async (req, res) => {
-	res.json({ message: 'success' });
+app.get('/api/recipes/search', async (req, res) => {
+	const searchTerm = req.query.searchTerm as string;
+	const page = parseInt(req.query.page as string);
+	const results = await RecipeAPI.searchRecipes(searchTerm, page);
+
+	return res.json(results);
+	// return res.json({ message: 'Hello from the backend!' });
 });
 
-app.listen(5550, () => {
-	console.log('Server running on localhost:5550');
-});
+app.listen(5550, () => console.log('Server running on port 5550'));
