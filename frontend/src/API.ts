0d1bcf3525f8a852219eb1313170973f5d526187
Chrisanto76 <antonypoure2@gmail.com>
2023-10-23 14:47:11 +0200

    make strain beetween backend and frontend

---
+++ frontend/src/API.ts
@@ -0,0 +1,10 @@
+export const searchRecipes = async (searchTerm: string, page: number) => {
+	const baseUrl = new URL('http://localhost:5550/api/recipes/search');
+	baseUrl.searchParams.append('searchTerm', searchTerm);
+	baseUrl.searchParams.append('page', page.toString());
+	const response = await fetch(baseUrl);
+	if (!response.ok) {
+		throw new Error(`HTTP error! status: ${response.status}`);
+	}
+	return await response.json();
+};
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,40 @@
-import React, { useState } from 'react';
+import React, { FormEvent, useState } from 'react';
+import * as api from './api';
+import { Recipe } from './types';
 import './App.css';
 
 const App = () => {
-	const [searchTerm, setSearchTerm] = useState('');
-	const [recipe, setRecipe] = useState({});
+	const [searchTerm, setSearchTerm] = useState<string>('');
+	const [recipes, setRecipes] = useState<Recipe[]>([]);
+
+	const handleSearchSubmit = async (event: FormEvent) => {
+		event.preventDefault();
+		try {
+			const recipes = await api.searchRecipes(searchTerm, 1);
+			setRecipes(recipes.results);
+		} catch (error) {
+			console.error(error);
+		}
+	};
 
 	return (
 		<div>
-			<h1>Hello World</h1>
+			<form onSubmit={(event) => handleSearchSubmit(event)}>
+				<input
+					type="text"
+					required
+					placeholder="Search for a recipe"
+					value={searchTerm}
+					onChange={(event) => setSearchTerm(event.target.value)}
+				></input>
+				<button type="submit">Submit</button>
+			</form>
+			{recipes.map((recipe) => (
+				<div>
+					recipe image location: {recipe.image}
+					recipe title: {recipe.title}
+				</div>
+			))}
 		</div>
 	);
 };
